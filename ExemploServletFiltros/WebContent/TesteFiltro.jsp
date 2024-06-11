15b45f0e01e0ca27153c904b94afeb653acd4413
neillane-carvalho <101107246+neillane-carvalho@users.noreply.github.com>
2024-06-11 19:44:49 -0300

    Add files via upload
    commit

---
+++ ExemploServletFiltros/WebContent/TesteFiltro.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form name="form" method="post" action="/ExemploServletFiltros/testeSemFiltro">
+
+	<table>
+		<tr>
+			<td>Nome</td>
+			<td><input type="text" name="nome"></td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center"><input type="submit" value="Enviar"></td>
+		</tr>	
+	
+	</table>
+
+
+</form>
+
+</body>
+</html>
---
+++ ExemploServletFiltros/src/usuario/UsuarioFiltro.java
@@ -0,0 +1,68 @@
+package usuario;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class UsuarioFiltro
+ */
+
+public class UsuarioFiltro implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public UsuarioFiltro() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+		//TUDO QUE VEM ANTES, SERÁ EXECUTADO ANTES DA SERVLET SER CHAMADA. NA IDA DA REQUISIÇÃO.
+		
+		String nome= request.getParameter("nome");
+		
+		if((nome!= null && !nome.equals("")) 
+				&& (nome.toUpperCase().equals("MARIA") || nome.toUpperCase().equals("JOAO"))){
+			
+			response.getWriter().append("Estou indo no meu caminho feliz! "); 
+			
+			chain.doFilter(request, response);
+			
+			response.getWriter().append("Estou VOLTANDO do meu caminho feliz! ");
+			
+			
+			
+		} else {
+			throw new IOException("Nome não aceito... ");
+		}
+
+		// pass the request along the filter chain
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
